d78e7c58e97c781467a759e2e594fd738dfde2a5
Shahed hossen <shahedhossensaad@gmail.com>
2019-02-04 00:08:46 +0600

    Add files via upload

---
+++ Leap Years.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    if(n%4==0 && n%400!=0)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+    return 0;
+}
