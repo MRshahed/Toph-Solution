b941f60d3d518f82364eae7a20ff160af3306c9b
Shahed hossen <shahedhossensaad@gmail.com>
2019-02-04 01:44:31 +0600

    Add files via upload

---
+++ Mount Everest.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,n;
+
+    cin>>t;
+
+    for(int i=0; i<t; i++)
+    {
+        cin>>n;
+
+        cout<<abs(8848-n)<<endl;
+    }
+    return 0;
+}
