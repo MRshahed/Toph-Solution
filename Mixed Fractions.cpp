1152c950a9bb931e0ca2d430752f66977527edb1
Shahed hossen <shahedhossensaad@gmail.com>
2019-02-03 23:52:59 +0600

    Add files via upload

---
+++ Mixed Fractions.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m;
+
+    cin>>n>>m;
+
+    int x=n/m;
+    int y=n-(x*m);
+
+    cout<<x<<" "<<y<<" "<<m<<endl;
+
+    return 0;
+}
